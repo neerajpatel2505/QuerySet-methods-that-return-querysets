5c506f0b1f5eefe60118c7e17588113f2cbdb227
Neeraj Kumar <nkurmbanshi@yahoo.com>
2024-02-28 13:28:23 +0530

    Query_Set

---
+++ project/app/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    stu_name = models.CharField(max_length=100)
+    stu_email = models.EmailField()
+    stu_city = models.CharField(max_length=50)
+    class Meta:
+        db_table = 'stu_table'
+        verbose_name_plural = "Student" 
+    
+    def __str__(self):
+        return self.stu_name
+    
+class Teacher(models.Model):
+    stu_name = models.CharField(max_length=100)
+    stu_email = models.EmailField()
+    stu_city = models.CharField(max_length=50)
+    class Meta:
+        db_table = 'tec_table'
+        verbose_name_plural = "Teacher" 
+    
+    def __str__(self):
+        return self.stu_name
